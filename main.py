bd879b0806e93c59b623701ddf6b39ba25991a25
Platoxa777 <pzhuman@inbox.ru>
2024-09-23 19:41:10 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, Router, types
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram import Router
 import asyncio
 
-# Защищённое хранение токена в коде
-API_TOKEN = "7531766827:AAGUy-ZymYI48RtnVjy3aTXPjFGbvcyDc-o"
+# Загрузка переменных окружения из файла .env
+load_dotenv()
 
-# Проверка на наличие токена
-if not API_TOKEN:
-    raise ValueError("Токен не задан! Проверьте переменную API_TOKEN в коде.")
+# Получение токена из переменной окружения
+API_TOKEN = os.getenv('7531766827:AAExZRKapjL3FMtxrOzoqRWkXM4mkeCdBDk')
+
+# Проверка, есть ли токен
+if API_TOKEN is None or API_TOKEN == '':
+    raise ValueError("No BOT_TOKEN provided. Please add it to your environment or .env file.")
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
+# Инициализация бота
 bot = Bot(token=API_TOKEN)
+
+# Инициализация диспетчера
 dp = Dispatcher()
+
+# Создание роутера
 router = Router()
+
+# Пример обработчика сообщений
+@router.message(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Добро пожаловать в моего Telegram бота.")
+
+# Регистрация роутера в диспетчере
 dp.include_router(router)
 
-# Обработчик команды /start
-@router.message(Command("start"))
-async def send_welcome(message: types.Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Запустить веб-приложение", web_app=WebAppInfo(url="https://platoxa777.github.io/schollboy/"))]
-    ])
-    await message.answer("Добро пожаловать в игру Schoolboy! Нажмите 'Запустить веб-приложение' для начала игры.", reply_markup=keyboard)
-
-# Основная функция для запуска поллинга
+# Асинхронная функция для запуска бота
 async def main():
     try:
-        logging.info("Запуск поллинга...")
+        await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
-    except asyncio.CancelledError:
-        logging.info("Поллинг был отменён.")
-    except Exception as e:
-        logging.error(f"Произошла ошибка: {e}")
     finally:
         await bot.session.close()
-        logging.info("Сессия бота завершена.")
 
+# Запуск бота
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logging.info("Бот был остановлен вручную.")
+    asyncio.run(main())
